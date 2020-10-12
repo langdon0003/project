c301b2ca59b34c508343c4552a900c053cd8cc5f
j <longnguyen16.ck27@st.ueh.edu.vn>
2020-10-12 13:27:25 +0700

    Add Helloword at /admin/poststest

--- api/core/urls.py
+++ api/core/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_view),
+    path('admin/poststest', views.home_view)
 ]
